2f4998ded37a0f1dae383dc403eb187195e35cb0
MFM_Fazrin <mmfazrin@phcc.gov.qa>
2022-08-13 15:04:13 +0300

    Add project files.

---
+++ EmailService.cs
@@ -0,0 +1,30 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.TransactionalEmails;
+
+namespace MailJetEmailService
+{
+    public class EmailService
+    {
+        private readonly IMailjetClient client = new MailjetClient("", "");
+
+        public async Task SendEmailAsync()
+        {
+            try
+            {
+                var email = new TransactionalEmailBuilder()
+                    .WithFrom(new SendContact("quadrate.lk@gmail.com"))
+                    .WithSubject("This is a sample email ")
+                    .WithHtmlPart("<h1>Hello World! <style color='Red'> This is a sample email </style></h1>")
+                    .WithTo(new SendContact("fazrinphcc@gmail.com"))
+                    .Build();
+
+                await client.SendTransactionalEmailAsync(email);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MailJetEmailService;
+
+Console.WriteLine("Sending Mail....");
+
+EmailService emailService = new ();
+await emailService.SendEmailAsync();
+Console.WriteLine("Mail Sent Successfully");
